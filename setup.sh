628e752f3c6c81e526ba8c694265838c2b88375b
Thomas Chemineau <tchemine@cisco.com>
2025-12-11 15:02:33 +0100

    Enforce git user

--- setup.sh
+++ setup.sh
@@ -7,3 +7,6 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 apt-get update && apt-get install -y git fonts-dejavu-core
 pip3 install -r "${script_dir}"/requirements.txt
+
+git config --global user.email "maanenfaces@tuta.io"
+git config --global user.name "Maanen Faces"
